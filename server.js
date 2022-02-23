cf1a5f081b87947d9280bff0d365c55426e12b8d
AlejandroAtehortua <alejoxx920810@gmail.com>
2022-02-23 00:39:30 -0500

    DeployHeroku vNode1

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/src'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/src/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`./src/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(process.env.PORT || 8080);
