AlejandroAtehortua/FrontendEstu

DeployHeroku vNode1

src/app/services/estudiante.service.ts
@@ -7,7 +7,7 @@ import { estudiante } from '../models/estudiante';
   providedIn: 'root'
 })
 export class EstudianteService {
-  myAppUrl = 'http://crudestudiantes.somee.com/';
+  myAppUrl = 'https://crudestudiantes.somee.com/';
   myApiUrl = 'api/estudiante/';
   list: estudiante[];
   private actualizarFormulario = new BehaviorSubject<estudiante>({} as any);

piedrahita1985/final

src/app/services/estudiante.service.ts
@@ -7,7 +7,7 @@ import { estudiante } from '../models/estudiante';
   providedIn: 'root'
 })
 export class EstudianteService {
-  myAppUrl = 'http://crudestudiantes.somee.com/';
+  myAppUrl = 'https://entrega.somee.com/';
   myApiUrl = 'api/estudiante/';
   list: estudiante[];
   private actualizarFormulario = new BehaviorSubject<estudiante>({} as any);